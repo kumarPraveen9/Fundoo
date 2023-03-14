ed2d97152b0e194478565538c22b89644dfe0b71
kumarPraveen9 <gpk50626@gmail.com>
2023-03-14 15:58:19 +0530

    added all the 4gateways  pages into fundoo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { LoginComponent } from './Components//login/login.component';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'login',component:LoginComponent},
+  {path:'sign-up',component:SignUpComponent},
+  {path:'reset',component:ResetPasswordComponent},
+  {path:'forget',component:ForgetPasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './Components/sign-up/sign-up.component';
+
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { LoginComponent } from './Components/login/login.component';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+import { ForgetPasswordComponent } from './Components/forget-password/forget-password.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
+    LoginComponent,
+    ResetPasswordComponent,
+    ForgetPasswordComponent,
+   
+   
    
    
   ],
